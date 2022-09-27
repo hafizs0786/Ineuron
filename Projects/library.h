196167fddb0781fdee4979a80976d33fb6e8938e
hafizs0786 <38181887+hafizs0786@users.noreply.github.com>
2022-09-27 19:03:37 +0530

    Delete library.h

--- Projects/library.h
+++
@@ -1,7 +0,0 @@
-double find_angle(double, double, double, double);
-
-double find_distance(double, double, double, double);
-
-double toRadian(double);
-
-double find_area(double, double, double, double, double, double);
